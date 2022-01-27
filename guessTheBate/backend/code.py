56613327e54b8207deae70c8480cae76cd483bfb
Ravi Prasad <jr.raviranjan@gmail.com>
2022-01-27 06:36:34 +0530

    First Commit

---
+++ guessTheBate/backend/code.py
@@ -0,0 +1,20 @@
+guess = 'e'
+a = 0
+while a==0 :
+    user_input = input("Guess the Character: ")
+    if(user_input == guess):
+        print("You Guessed it Right!")
+        a=1
+        break
+    else:
+        print("Ops, Try Again")
+
+    play_again = input("Wanna Play Again(Y/N)?")
+    if ((play_again == "Y") or (play_again == "y") ):
+        a = 0
+    else:
+        a = 1
+        print("Alright, GG")
+
+# to do
+# Dynamic guess.
